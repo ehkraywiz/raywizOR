5bd7470b3c9b663fe36cecdf3e5c6daad8ecb123
ehkraywiz <eirik@raywiz.com>
2026-03-17 08:12:45 +0100

    Use versioning

--- bygg.ps1
+++ bygg.ps1
@@ -1,11 +1,20 @@
+param($tag)
+$env:OR_IMAGE_TAG = $null
 docker compose down
-docker compose down # Stopper to ganger for å garantere at det er nede
+docker compose down
+
 
 ./gradlew clean installDist #clean for å rense cache, installDist er navnet på gradle tasken som faktisk bygger Manager containeren.
 
 if ($LASTEXITCODE -eq 0) #if build ok
 {
-    docker build -t eirikraywiz/openremote:custom ./manager/build/install/manager # bygg docker imaget med navn openremote og tag custom FRA mappen "gradle installDist" legger builden.
+    if($PSBoundParameters.ContainsKey("tag")) {
+    $env:OR_IMAGE_TAG = $tag
+    docker build -t eirikraywiz/openremote:$($tag) ./manager/build/install/manager # bygg docker imaget med navn openremote og tag custom FRA mappen "gradle installDist" legger builden.
+    }
+    else {
+        docker build -t eirikraywiz/openremote:custom ./manager/build/install/manager # bygg docker imaget med navn openremote og tag custom FRA mappen "gradle installDist" legger builden.
+    }
     if ($LASTEXITCODE -eq 0)
     {
         docker compose up -d # start opp
