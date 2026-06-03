cbf2a675125d2d6fc3d2e90e0193651132dd9033
ehkraywiz <eirik@raywiz.com>
2026-06-03 15:39:50 +0200

    zoneminder v1, meh

--- bygg.ps1
+++ bygg.ps1
@@ -1,15 +1,22 @@
-param($tag)
+param(
+    [string]$tag,
+    [switch]$skip
+)
 $env:OR_IMAGE_TAG = $null
 docker compose down
-#docker image prune -f
 
+if (-not $skip)
+{
 ./gradlew clean installDist #clean for å rense cache, installDist er navnet på gradle tasken som faktisk bygger Manager.
+}
 
 if ($LASTEXITCODE -eq 0) #if build ok
 {
     if($PSBoundParameters.ContainsKey("tag")) {
-    $env:OR_IMAGE_TAG = $tag
-    docker build -t eirikraywiz/openremote:$($tag) ./manager/build/install/manager # bygg docker imaget med navn openremote og tag custom FRA directory "gradle installDist" legger builden.
+        $env:OR_IMAGE_TAG = $tag
+        if(-not $skip){
+            docker build -t eirikraywiz/openremote:$($tag) ./manager/build/install/manager # bygg docker imaget med navn openremote og tag custom FRA directory "gradle installDist" legger builden.
+        }
     }
     else {
         docker build -t eirikraywiz/openremote:custom ./manager/build/install/manager
